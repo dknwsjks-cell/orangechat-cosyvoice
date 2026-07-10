f93a4a068bf33acbbb945fa879806cbf5595a66f
xiaoyu <xiaoyu@example.com>
2026-07-10 20:04:21 +0800

    fix: 修复CI构建配置，子模块改用HTTPS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,8 +18,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         maven("https://jitpack.io")
